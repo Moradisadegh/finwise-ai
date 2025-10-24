069e55f01ca97bed941df8dc128bbeaade4dbbcb
Moradisadegh <42092639+Moradisadegh@users.noreply.github.com>
2025-10-24 20:16:20 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,1 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  images: {
+    domains: ['your-supabase-url.supabase.co'],
+  },
+}
 
+module.exports = nextConfig
